e645596fb3db663976f562443c1b36629daad55b
Naperehvate <tropikano647@mail.ru>
2024-06-03 12:27:00 +0500

    rework class ander the DB

--- src/main/java/Persons_collect.java
+++ src/main/java/Persons_collect.java
@@ -1,2 +1,28 @@
-public class Persons_collect {
+public class Persons_collect
+{
+    public Persons_collect()
+    {
+       DataBaseHelper.CreateNewDataBase();
+       DataBaseHelper.CreatePersonTable();
+    }
+    public boolean addPerson(Persons person)
+    {
+
+    }
+    public boolean removePerson(Persons person)
+    {
+
+    }
+    public boolean updatePerson(Persons person)
+    {
+
+    }
+    public void PrintAllPersons()
+    {
+
+    }
+    public Persons SearchPersonsByIdNumber(String idNumber)
+    {
+
+    }
 }
